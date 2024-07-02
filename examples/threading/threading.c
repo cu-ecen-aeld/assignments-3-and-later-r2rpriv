f5958282726616c24eb03625a8c678e92ffe3008
r2rpriv <rometoroam@protonmail.com>
2024-07-01 23:26:15 -0400

    Assignment 4.1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,24 +10,26 @@
 
 void* threadfunc(void* thread_param)
 {
-
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data *thrdata = (struct thread_data*)thread_param;
+    usleep(thrdata->wto_ms * 1000);
+    pthread_mutex_lock(thrdata->mtx);
+    usleep(thrdata->wtr_ms * 1000);
+    pthread_mutex_unlock(thrdata->mtx);
+    thrdata->thread_complete_success =  true;
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data *thread_args = (struct thread_data *) malloc(sizeof(struct thread_data));
+    thread_args->wto_ms = wait_to_obtain_ms;
+    thread_args->wtr_ms = wait_to_release_ms;
+    thread_args->mtx = mutex;
+
+    if( pthread_create(thread, NULL, &threadfunc,(void *)thread_args) !=0 )
+    	return false;
+    else
+	return true;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -8,12 +8,10 @@
  * the joiner thread.
  */
 struct thread_data{
-    /*
-     * TODO: add other values your thread will need to manage
-     * into this structure, use this structure to communicate
-     * between the start_thread_obtaining_mutex function and
-     * your thread implementation.
-     */
+    pthread_mutex_t *mtx;
+
+    int wto_ms;
+    int wtr_ms;
 
     /**
      * Set to true if the thread completed with success, false
